bcd204ffa78a078a65d3893dd84a1e42b8e0306a
Keerthana-n37 <keerthana3739@gmail.com>
2026-04-16 21:59:59 +0530

    array

---
+++ FindDuplicate.java
@@ -0,0 +1,17 @@
+public class FindDuplicate 
+{
+    public static void main(String[] args) 
+	{
+        int[] arr = {2, 3, 4, 2, 6, 5};
+
+        System.out.print("Duplicate elements: ");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
